112cc330a7870590f7f3e9ce3328d8d075c0781d
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-02 17:35:16 -0300

    removing test code

--- src/index.js
+++ src/index.js
@@ -15,39 +15,7 @@ app.get('/',(req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-    var quickReply = req.entry.messaging.message.text;
-    var intentName = req.body.queryResult.intent.displayName;
-
-    if(quickReply == "Esportes") {
-        return res.json({"fulfillmentText": "esportes"})
-    } 
-    // else {
-    //     return res.json({
-    //         "fulfillmentMessages": [
-    //           {
-    //             "quickReplies": {
-    //               "title": "Sobre qual tema você deseja se atualizar?",
-    //               "quickReplies": [
-    //                 "Esportes",
-    //                 "Política",
-    //                 "Entretenimento",
-    //                 "Famosos"
-    //               ]
-    //             },
-    //             "platform": "FACEBOOK"
-    //           },
-    //           {
-    //             "text": {
-    //               "text": [
-    //                 "Olá! Eu sou o Casper, seu assistente virtual. Aqui trago as mais variadas notícias para você se atualizar."
-    //               ]
-    //             }
-    //           }
-    //         ] 
-    //       }  
-    //   )
-    // }
-    
+    return res.json({"fulfillmentText": "esportes"})
 })
 
 app.listen(process.env.PORT || 3333);

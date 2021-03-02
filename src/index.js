bcfe4a5d8af1fb87f7845e227ee579ac9f3ab10a
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-02 15:01:26 -0300

    adding new fulfillment response

--- src/index.js
+++ src/index.js
@@ -15,30 +15,39 @@ app.get('/',(req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-    return res.json({
-          "fulfillmentMessages": [
-            {
-              "quickReplies": {
-                "title": "Sobre qual tema você deseja se atualizar?",
-                "quickReplies": [
-                  "Esportes",
-                  "Política",
-                  "Entretenimento",
-                  "Famosos"
-                ]
-              },
-              "platform": "FACEBOOK"
-            },
-            {
-              "text": {
-                "text": [
-                  "Olá! Eu sou o Casper, seu assistente virtual. Aqui trago as mais variadas notícias para você se atualizar."
-                ]
-              }
-            }
-          ] 
-        }  
-    )
+    var quickReply = request.entry.messaging.message.text;
+    var intentName = request.body.queryResult.intent.displayName;
+
+    if(quickReply == "Esportes") {
+        return res.json({"fulfillmentMessages": "esportes"})
+    } 
+    // else {
+    //     return res.json({
+    //         "fulfillmentMessages": [
+    //           {
+    //             "quickReplies": {
+    //               "title": "Sobre qual tema você deseja se atualizar?",
+    //               "quickReplies": [
+    //                 "Esportes",
+    //                 "Política",
+    //                 "Entretenimento",
+    //                 "Famosos"
+    //               ]
+    //             },
+    //             "platform": "FACEBOOK"
+    //           },
+    //           {
+    //             "text": {
+    //               "text": [
+    //                 "Olá! Eu sou o Casper, seu assistente virtual. Aqui trago as mais variadas notícias para você se atualizar."
+    //               ]
+    //             }
+    //           }
+    //         ] 
+    //       }  
+    //   )
+    // }
+    
 })
 
 app.listen(process.env.PORT || 3333);

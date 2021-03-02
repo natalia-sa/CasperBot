d41a16f119e6318c29e831fe48c9c02dd534fb48
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-02 13:33:24 -0300

    Adding /webhook route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const cors = require('cors');
 
+
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/',(req, res) => {
     return res.json({message: 'hello'});
+})
+
+app.post('/webhook', (req, res) => {
+    return res.json({"fulfillmentText": "text"})
 })
 app.listen(process.env.PORT || 3333);

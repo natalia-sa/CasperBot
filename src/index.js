9a33e4dff38b532a61095ec7cec857c7162d72c9
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-03 11:29:54 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://casper1:casper1@omnistack.oq54w.mongodb.net/casperbot?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 const cors = require('cors');
+
+app.post('/webhook', (req, res) => {
+    var intentName = req.body.queryResult.intent.displayName;
+    
+    if(intentName == "Default Welcome Intent - Política") {
+        res.json({"fulfillmentText":"Notícias sobre poĺitica"});
+        
+    } else if(intentName == "Default Welcome Intent - Entretenimento") {
+        res.json({"fulfillmentText":"Notícias sobre entretenimento"});
+        
+    } else if(intentName == "Default Welcome Intent - Famosos") {
+        res.json({"fulfillmentText":"Notícias sobre famosos"});
+        
+    } else if(intentName == "Default Welcome Intent - Esportes") {
+        res.json({"fulfillmentText":"Notícias sobre esportes"});
+    }
+})
 
 app.use(routes);
 app.use(cors());
--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,12 @@
 const express = require('express')
 const routes = express.Router();
+const SessionController = require('./controllers/SessionController');
 
 routes.get('/',(req, res) => {
     return res.json({message: 'hello'});
 })
 
-routes.post('/webhook', (req, res) => {
-    var intentName = req.body.queryResult.intent.displayName;
-    
-    if(intentName == "Default Welcome Intent - Política") {
-        res.json({"fulfillmentText":"Notícias sobre poĺitica"});
-        
-    } else if(intentName == "Default Welcome Intent - Entretenimento") {
-        res.json({"fulfillmentText":"Notícias sobre entretenimento"});
-        
-    } else if(intentName == "Default Welcome Intent - Famosos") {
-        res.json({"fulfillmentText":"Notícias sobre famosos"});
-        
-    } else if(intentName == "Default Welcome Intent - Esportes") {
-        res.json({"fulfillmentText":"Notícias sobre esportes"});
-    }
-})
+routes.post('/users', SessionController.store);
+
 
 module.exports = routes;

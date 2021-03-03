1061b3d1e8774649c7517bef3afcdc0e4f14eb4c
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-03 11:41:30 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 
@@ -12,7 +12,7 @@ mongoose.connect('mongodb+srv://casper1:casper1@omnistack.oq54w.mongodb.net/casp
 
 const cors = require('cors');
 
-app.use(routes);
+// app.use(routes);
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));

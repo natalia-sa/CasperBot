14ae0d7c4e382a5682bd504ac44f0df597b50d9c
Natália Salvino <natalia.andre@ccc.ufcg.edu.br>
2021-03-08 13:31:35 -0300

    removing unecessary logic

--- src/controllers/BotController.js
+++ src/controllers/BotController.js
@@ -4,24 +4,11 @@ const BotFunctions = require('../Util/Botfunctions');
 module.exports = {
     
     async botResponse(req, res) {
-        var intentName = req.body.queryResult.intent.displayName;
+        var theme = req.body.queryResult.queryText;
         
         let cards = [];
-        var posts;
+        var posts = await PostController.filterByTheme(theme)
 
-        if(intentName == "Default Welcome Intent - Política") {
-            posts = await PostController.filterByTheme("Politica")
-
-        } else if(intentName == "Default Welcome Intent - Entretenimento") {
-            posts = await PostController.filterByTheme("Entretenimento")
-            
-        } else if(intentName == "Default Welcome Intent - Famosos") {
-            posts = await PostController.filterByTheme("Famosos")
-            
-        } else if(intentName == "Default Welcome Intent - Esportes") {
-            posts = await PostController.filterByTheme("Esportes")
-            
-        }
         Array.from(posts).forEach((post) => {
             cards.push(BotFunctions.createCard(post.title,post.description,post.imageUrl,post.link))} )
         res.json({"fulfillmentMessages": [{"payload": BotFunctions.createCarousel(cards)}]});
